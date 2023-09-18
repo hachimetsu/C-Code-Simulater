a14526c74c5915dfad927381e94233eb329fd6c0
Hachi Metsu <hachimetsu@gmail.com>
2023-09-18 14:59:05 +0000

    testing merge on dev

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
-
+import Image from 'next/image';
+import React from "react";
 export default function Home() {
   return (
     <div className="workspace w-screen h-screen flex justify-center items-center">
