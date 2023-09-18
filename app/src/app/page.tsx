823282934c82b07f0bdebd8aa68506de1c9c1f0a
Hachi Metsu <hachimetsu@gmail.com>
2023-09-18 14:57:35 +0000

    testing merge

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="workspace"></div>
+    <div className="workspace w-screen h-screen flex justify-center items-center">
+      <span>Workspace</span>
+    </div>
   )
 }
